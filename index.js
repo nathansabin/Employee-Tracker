c0767cfdf42ec3bba5000f50e7c824a346ed7509
nathansabin <sabinnat001@gmail.com>
2023-06-11 12:30:10 -0700

    bug fix + started update function

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // grabs object out of user_interface file
 const DataInput = require("./lib/user_interface");
 const user = new DataInput();
+const functionList = [user.showDepartments, user.showRoles, user.showEmployee, user.departments, user.roles, user.employees];
 
 // routes user to desired function
-async function selector() {
-    return console.log("hello, world");
+async function selector(selection) {
+    for (var i = 0; i < user.choice.length; i++) {
+        if (selection === user.choice[i]) {
+            functionList[i]().then(() =>{
+                return init();
+            });
+        }
+    }
 }
 
 // function to initialize
 async function init(){
-    while (true) {
-        await user.setQuerry()
-        .then((results) => {
-            return selector(results.route);
-        });
-    }
+    await user.setQuerry()
+    .then((results) => {
+        return selector(results.route);
+    });
 }
 
 // starts app
