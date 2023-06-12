77217ec750b1c14b008ae5ea2fa53bdf1d4155a4
nathansabin <sabinnat001@gmail.com>
2023-06-11 22:27:38 -0700

    bug fixed just need to fix update function

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // grabs object out of user_interface file
 const DataInput = require("./lib/user_interface");
 const user = new DataInput();
-const functionList = [user.showDepartments, user.showRoles, user.showEmployee, user.departments, user.roles, user.employees];
+const functionList = [user.showDepartments, user.showRoles, user.showEmployee, user.departments, user.roles, user.employees, user.updatesEmployee];
 
 // routes user to desired function
 async function selector(selection) {
     for (var i = 0; i < user.choice.length; i++) {
         if (selection === user.choice[i]) {
-            functionList[i]().then(() =>{
-                return init();
-            });
+            await functionList[i]();
+                return true;
         }
     }
+    return false;
 }
 
 // function to initialize
 async function init(){
-    await user.setQuerry()
-    .then((results) => {
-        return selector(results.route);
-    });
+    let continueApp = true;
+    while (continueApp) {
+        await user.setQuerry().then(async (results) => {
+            constinueApp = await selector(results.route);
+        })
+    }
 }
 
 // starts app
